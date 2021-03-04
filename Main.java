3dc8bc5d1c86ade327150ecf5d14d2dd3b199115
andrea Lepori <57524583+andrealepori@users.noreply.github.com>
2021-03-04 14:21:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+   
+   String nome= "Andrea";
+   
+   
+   int anni =22;
+   int natoNel= 1997;
+   double altezza =176.33;
+   
+   System.out.println("IL mio nome è " + nome + "sono nato nel" + natoNel);
+  }
+}

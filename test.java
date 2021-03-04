7a67b7b233d02b6191c740909bdcd1ba5b673ad8
andrea Lepori <57524583+andrealepori@users.noreply.github.com>
2021-03-04 14:58:20 +0000

    test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+  
+public class Test {
+    public static void main(Stings args[])
+
+    {
+
+      String name= "Andrea";
+      char inizialenome = 'A';
+
+    int anni = 22;
+    int natoNel = 1997;
+    duble altezza= 167.59;
+    }
+  }

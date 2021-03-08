a2254fec79c37141da11aa44bac50d8f6044fe6a
andrea Lepori <57524583+andrealepori@users.noreply.github.com>
2021-03-08 16:14:55 +0000

    funzioni

---
+++ Array.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    public static void main(String args[]) {
+   
+    String[] cassettiera = new string [6];
+    
+    cassettiera [0] = "scarpa 0";
+    cassettiera [1] = "scarpa 1";
+    cassettiera [2] = "scarpa 2";
+    cassettiera [3] = "scarpa 3";
+    cassettiera [4] = "scarpa 4";
+    cassettiera [5] = "scarpa 5";
+
+
+  System.out.print("cassetto n.1" + cassettiera[0]);
+
+    }
+}
---
+++ funzioni.java
@@ -0,0 +1,14 @@
+
+
+public class funzioni {
+   static void pro(String nome, String nome2)
+   {  
+  System.out.print ("nome" +nome+"+"+nome2);
+   } 
+  public static void main(String[] args) {
+      int a =5;
+            pro("andrea", "Luca");
+    }
+  }
+
+
